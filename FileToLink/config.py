04a8756df7444f075e35862ffc4d3f46e7b5342d
DropLink.co <98651207+Jdiskin@users.noreply.github.com>
2022-01-30 10:41:23 +0530

    Update config.py

--- FileToLink/config.py
+++ FileToLink/config.py
@@ -34,7 +34,6 @@ class Strings:
     start = Config.Start_Message
     dl_link = "🔗 DOWNLOAD LINK 🔗"
     st_link = "🔗 STREAMING LINK 🔗"
-
     generating_link = "**🔼 Uploading fast Just Wait...\nSpeed: 100.36 MiB/s**"
     bot_channel = "💲 SIGN UP 💲"
     dev_channel = "💲 SIGN UP 💲"
